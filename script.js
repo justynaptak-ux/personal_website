8709127b3f97921d74db0444d029e9326aec8481
justynaptak-ux <justyna.ptak.ux@gmail.com>
2019-07-19 13:59:09 +0200

    1st commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+  $(".circle").each( function() {
+    $(this).height($(this).width())
+  });
+
+  $(".circle").hover(function() {
+    $(this).children("a").children(".afterhover").fadeIn( "slow" );
+  },function() {
+    $(this).children("a").children(".afterhover").fadeOut( "slow" );
+  });
+
+  $(window).resize(function(){
+    $(".circle").each( function() {
+      $(this).height($(this).width())
+    });
+  });
+
+});
